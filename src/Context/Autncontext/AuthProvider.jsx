56c5c75448df5f1590ac3038629e672640e46935
Ariful Islam <arifulq234@gmail.com>
2025-11-24 23:12:08 +0600

    Authorization key send to server by header

--- src/Context/Autncontext/AuthProvider.jsx
+++ src/Context/Autncontext/AuthProvider.jsx
@@ -10,48 +10,48 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../../firebase/firebase";
-const googleProvider=new GoogleAuthProvider()
+const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({ children }) => {
-    const [user,setUser]=useState(null)
-    const [isLoading,setIsloading]=useState(true)
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsloading] = useState(true);
   const CreateUser = (email, password) => {
-    setIsloading(true)
+    setIsloading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const signInUser = (email, password) => {
-        setIsloading(true)
+    setIsloading(true);
 
     return signInWithEmailAndPassword(auth, email, password);
   };
-const googleSocialLogin=()=>{
-        setIsloading(true)
+  const googleSocialLogin = () => {
+    setIsloading(true);
 
-   return signInWithPopup(auth,googleProvider)
-}
-const logOut=()=>{
-   return signOut(auth)
-}
-const UpdateUserProfile=(UpdatedInfo)=>{
-  return  updateProfile(auth.currentUser,UpdatedInfo)
-}
-useEffect(()=>{
-    const unsubcribe=onAuthStateChanged(auth,(currentUser)=>{
-        setUser(currentUser)
-        setIsloading(false)
-    })
-    return ()=>{
-        unsubcribe()
-    }
-},[])
+    return signInWithPopup(auth, googleProvider);
+  };
+  const logOut = () => {
+    return signOut(auth);
+  };
+  const UpdateUserProfile = (UpdatedInfo) => {
+    return updateProfile(auth.currentUser, UpdatedInfo);
+  };
+  useEffect(() => {
+    const unsubcribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+
+      setIsloading(false);
+    });
+    return () => {
+      unsubcribe();
+    };
+  }, []);
   const AuthInfo = {
     CreateUser,
     signInUser,
-   googleSocialLogin,
-   logOut,
-   user,
-   isLoading,
-   UpdateUserProfile
-
+    googleSocialLogin,
+    logOut,
+    user,
+    isLoading,
+    UpdateUserProfile,
   };
   return <Authcontext value={AuthInfo}>{children}</Authcontext>;
 };
--- src/hooks/SecureInstance.jsx
+++ src/hooks/SecureInstance.jsx
@@ -1,9 +1,17 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
+import useAuthhooks from './Authhooks';
 const secureInstance=axios.create({
     baseURL:'http://localhost:3000'
 })
 const useSecureInstance = () => {
+    const {user}=useAuthhooks()
+  useEffect(()=>{
+      secureInstance.interceptors.request.use((config)=>{
+          config.headers.Authorization= `Bearer ${user.accessToken}`
+        return config
+    })
+  },[user])
     return secureInstance;
 };
 
