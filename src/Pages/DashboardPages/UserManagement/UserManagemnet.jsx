eec4d44535bde6f24234ca5e72a1138740dfff40
Ariful Islam <arifulq234@gmail.com>
2025-11-26 03:12:11 +0600

    users page added on dashboard

--- src/Pages/DashboardPages/UserManagement/UserManagemnet.jsx
+++ src/Pages/DashboardPages/UserManagement/UserManagemnet.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import useSecureInstance from "../../../hooks/SecureInstance";
 
 const UserManagemnet = () => {
-    return (
-        <div>
-            users
-        </div>
-    );
+  const Instance = useSecureInstance();
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await Instance.get("/users");
+
+      return res.data;
+    },
+  });
+
+  return (
+    <div>
+      <h1 className="text-3xl text-neutral">User {users.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <td>#</td>
+              <th>Photo</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={user.photoURL}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>{user.displayName}</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <th>
+                  <button className="btn btn-primary text-black btn-xs">details</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default UserManagemnet;
