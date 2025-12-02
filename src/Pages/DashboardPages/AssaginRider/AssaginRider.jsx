7109ebf41b7675e0179c068c0a663116207d5ac5
Ariful Islam <arifulq234@gmail.com>
2025-12-02 23:22:43 +0600

    assaign rider and open modal complete

--- src/Pages/DashboardPages/AssaginRider/AssaginRider.jsx
+++ src/Pages/DashboardPages/AssaginRider/AssaginRider.jsx
@@ -1,20 +1,78 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import useSecureInstance from '../../../hooks/SecureInstance';
+import { useQuery } from "@tanstack/react-query";
+import React, { useRef } from "react";
+import useSecureInstance from "../../../hooks/SecureInstance";
 
 const AssaginRider = () => {
-    const Instance=useSecureInstance()
-    const {data:pendingParcels=[]}=useQuery({
-        queryKey:['paid-Parcels'],
-        queryFn:()=> Instance.get(`/parcels`,{params:{deliveryStatus:"Pickup in progress"}}).then((res)=>res.data)
-    
-        
-    })
-    return (
-        <div>
-            {pendingParcels.length}
+  const Instance = useSecureInstance();
+  const modalRef=useRef()
+  const { data: pendingParcels = [] } = useQuery({
+    queryKey: ["paid-Parcels"],
+    queryFn: () =>
+      Instance.get(`/parcels`, {
+        params: { deliveryStatus: "Pickup in progress" },
+      }).then((res) => res.data),
+  });
+const handleModal=(parcel)=>{
+    modalRef.current.showModal()
+}
+  return (
+    <div>
+      <h1>Total Pending Parcesl{pendingParcels.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>SenderName</th>
+              <th>Tracking Id</th>
+              <th>Sender District</th>
+              <th>Sender Phone</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pendingParcels.map((parcel, index) => (
+              <tr key={index}>
+                <th>{index + 1}</th>
+                <td>{parcel.SenderName}</td>
+                <td>{parcel.TracingId}</td>
+                <td>{parcel.senderDistrict}</td>
+                <td>{parcel.SenderPhone}</td>
+
+                <td>
+                  <button onClick={()=>handleModal(parcel)} className="btn btn-primary text-neutral">
+                    Assaing Rider
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      {/* <button
+        className="btn"
+        onClick={() => document.getElementById("my_modal_5").showModal()}
+      >
+        open modal
+      </button> */}
+      <dialog ref={modalRef} className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
         </div>
-    );
+      </dialog>
+    </div>
+  );
 };
 
 export default AssaginRider;
