bf79ea6c8a08b2eab9b4f0a1ca914efb918cd4f0
Ariful Islam <arifulq234@gmail.com>
2025-12-03 15:40:45 +0600

    Delivery status functionality added

--- src/Pages/DashboardPages/AssaigendDelivery/AssaignedDelivery.jsx
+++ src/Pages/DashboardPages/AssaigendDelivery/AssaignedDelivery.jsx
@@ -1,69 +1,114 @@
-import React from 'react';
-import useAuthhooks from '../../../hooks/Authhooks';
-import useSecureInstance from '../../../hooks/SecureInstance';
-import { useQuery } from '@tanstack/react-query';
-import Swal from 'sweetalert2';
+import React from "react";
+import useAuthhooks from "../../../hooks/Authhooks";
+import useSecureInstance from "../../../hooks/SecureInstance";
+import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 const AssaignedDelivery = () => {
-    const {user}=useAuthhooks()
-    const Instance=useSecureInstance()
-    const{data:pendingdeliveries=[],refetch:deliveryTaskRefetch}=useQuery({
-        queryKey:['pending-percels','Rider assainge',user?.email],
-        queryFn:async()=>{
-           const res=await Instance.get(`/parcels/rider?rideremail=${user.email}&deliverystatus=Rider%20assainge`)
-           return res.data
-        
-        }
-    })
-    // console.log(pendingdeliveries)
-    const handleAcceptDelivery=(deliveryTask)=>{
-     const updatedStatus={deliveryStatus:'Rider_Arriving'}
-      Instance.patch(`parcels/${deliveryTask._id}/deleveryStatus`,updatedStatus).then(res=>{
-        console.log(res.data)
-         if (res.data.modifiedCount) {
-          deliveryTaskRefetch()
-                    Swal.fire({
-                      title: "Update",
-                      text: `Delivery Accpepted`,
-                      icon: "success",
-                    });
-           }
-      })
-    }
-    return (
-        <div>
-            <h1 className='text-4xl text-neutral '>Total pending Delivery: {pendingdeliveries.length}</h1>
-            <div className="overflow-x-auto">
-  <table className="table table-zebra">
-    {/* head */}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Parcel Name</th>
-        <th>SenderPhone</th>
-        <th>Sender Address</th>
-        <th>Confirm</th>
-      </tr>
-    </thead>
-    <tbody>
-      {pendingdeliveries.map((deliveryTask,i)=><tr key={deliveryTask._id}>
-        <th>{i+1}</th>
-        <td>{deliveryTask.parcelName}</td>
-        <td>{deliveryTask.SenderPhone}</td>
-        <td>{deliveryTask.SenderAddress}</td>
-        <td className=' flex gap-2'>
-            <button onClick={()=>handleAcceptDelivery(deliveryTask)} className='btn btn-primary text-black'>Accept</button>
-            <button className='btn btn-warning text-black'>Reject</button>
-        </td>
-      </tr>)}
-      
-     
-  
-    </tbody>
-  </table>
-</div>
-        </div>
-    );
+  const { user } = useAuthhooks();
+  const Instance = useSecureInstance();
+  const { data: pendingdeliveries = [], refetch: deliveryTaskRefetch } =
+    useQuery({
+      queryKey: ["pending-percels", "Rider assainge", user?.email],
+      queryFn: async () => {
+        const res = await Instance.get(
+          `/parcels/rider?rideremail=${user.email}&deliverystatus=Rider%20assainge`
+        );
+        return res.data;
+      },
+    });
+  // console.log(pendingdeliveries)
+  const handleAcceptDelivery = (deliveryTask, status) => {
+    const updatedStatus = { deliveryStatus: status };
+    Instance.patch(
+      `parcels/${deliveryTask._id}/deleveryStatus`,
+      updatedStatus
+    ).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount) {
+        deliveryTaskRefetch();
+        Swal.fire({
+          title: "Update",
+          text: `Delivery Accpepted`,
+          icon: "success",
+        });
+      }
+    });
+  };
+  return (
+    <div>
+      <h1 className="text-4xl text-neutral ">
+        Total pending Delivery: {pendingdeliveries.length}
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Parcel Name</th>
+              <th>SenderPhone</th>
+              <th>Sender Address</th>
+              <th>Confirm</th>
+              <th>Update Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pendingdeliveries.map((deliveryTask, i) => (
+              <tr key={deliveryTask._id}>
+                <th>{i + 1}</th>
+                <td>{deliveryTask.parcelName}</td>
+                <td>{deliveryTask.SenderPhone}</td>
+                <td>{deliveryTask.SenderAddress}</td>
+                <td className=" flex gap-2">
+                  {deliveryTask.deliveryStatus === "Rider assainge" ? (
+                    <>
+                      {" "}
+                      <button
+                        onClick={() =>
+                          handleAcceptDelivery(deliveryTask, "Rider_Arriving")
+                        }
+                        className="btn btn-primary text-black"
+                      >
+                        Accept
+                      </button>
+                      <button className="btn btn-warning text-black">
+                        Reject
+                      </button>
+                    </>
+                  ) : (
+                    <h1>Accepted</h1>
+                  )}
+                </td>
+                <td>
+                  {deliveryTask.deliveryStatus !== "Rider assainge" ? (
+                    <div className="flex gap-2">
+                      <button
+                        onClick={() => {
+                          handleAcceptDelivery(deliveryTask, "Picked_up");
+                        }}
+                        className={`btn btn-primary text-black`}
+                        disabled={deliveryTask.deliveryStatus ==="Rider assainge"?false:true}
+                      >
+                        {deliveryTask.deliveryStatus==="Rider assainge"?"Pick up":"Alredy PikedUp"}
+                      </button>
+                      <button onClick={() => {
+                          handleAcceptDelivery(deliveryTask, "Deliveried");
+                        }} className="btn btn-primary text-black">
+                        Deliveried
+                      </button>
+                    </div>
+                  ) : (
+                    <button className="btn">Not yet Accepted</button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AssaignedDelivery;
